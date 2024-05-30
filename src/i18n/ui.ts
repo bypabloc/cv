5a128886eaeba796cee6ddce801815b185557198
Pablo Contreras <pacg1991@gmail.com>
2024-05-29 20:17:10 -0500

    feat: keys translations added

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,19 +1,82 @@
-export const languages = {
-  en: "English",
-  es: "Español",
-};
-
 export const showDefaultLang = false;
 
 export const defaultLang = "es";
 
 export const ui = {
-  en: {
-    "nav.home": "Home",
-    "nav.about": "About",
-  },
   es: {
-    "nav.home": "Inicio",
-    "nav.about": "Acerca de",
+    "about-me": {
+      title: "Sobre mí",
+    },
+    awards: {
+      title: "Premios",
+    },
+    "work-experience": {
+      title: "Experiencia laboral",
+    },
+    education: {
+      title: "Educación",
+    },
+    projects: {
+      title: "Proyectos",
+    },
+    certifications: {
+      title: "Certificados",
+    },
+    publications: {
+      title: "Publicaciones",
+    },
+    technicalSkills: {
+      title: "Habilidades técnicas",
+    },
+    softSkills: {
+      title: "Habilidades blandas",
+    },
+    languages: {
+      title: "Idiomas",
+    },
+    interests: {
+      title: "Intereses",
+    },
+    references: {
+      title: "Referencias",
+    },
+  },
+  en: {
+    "about-me": {
+      title: "About me",
+    },
+    awards: {
+      title: "Awards",
+    },
+    "work-experience": {
+      title: "Work experience",
+    },
+    education: {
+      title: "Education",
+    },
+    projects: {
+      title: "Projects",
+    },
+    certifications: {
+      title: "Certifications",
+    },
+    publications: {
+      title: "Publications",
+    },
+    technicalSkills: {
+      title: "Technical skills",
+    },
+    softSkills: {
+      title: "Soft skills",
+    },
+    languages: {
+      title: "Languages",
+    },
+    interests: {
+      title: "Interests",
+    },
+    references: {
+      title: "References",
+    },
   },
 } as const;
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -6,11 +6,37 @@ export function getLangFromUrl(url: URL) {
   return defaultLang;
 }
 
-export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
+export function useTranslations(lang: string = defaultLang) {
+  return function t(key: string) {
+    const keys = key.split("."); // Divide la cadena por puntos
+    let translation = ui[lang];
+
+    for (const k of keys) {
+      if (translation[k] !== undefined) {
+        translation = translation[k]; // Navega por los niveles del objeto
+      } else {
+        translation = undefined;
+        break;
+      }
+    }
+
+    // Si no se encuentra la traducción en el idioma seleccionado, busca en el idioma por defecto
+    if (translation === undefined) {
+      translation = ui[defaultLang];
+      for (const k of keys) {
+        if (translation[k] !== undefined) {
+          translation = translation[k];
+        } else {
+          translation = undefined;
+          break;
+        }
+      }
+    }
+
+    return translation;
   };
 }
+
 
 export function useTranslatedPath(lang: keyof typeof ui) {
   return function translatePath(path: string, l: string = lang) {
